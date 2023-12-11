a61fc2b4d3faef73affea9b0db12df7f0ad83277
ajrfyd <61442171+ajrfyd@users.noreply.github.com>
2023-12-11 15:34:28 +0900

    Update: json file

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-  origin: ["http://localhost:5173", "https://k-log3943.netlify.app"],
+  origin: ["http://localhost:5173", "https://k-log3943.netlify.app", "http://localhost:5174"],
   method: ["GET", "POST", "DELETE", "OPTIONS"],
   credentials: true,
 }));
